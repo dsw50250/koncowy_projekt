3d82668ab2b6af3b1df752307486a1171ca575ed
dsw50250 <129311304+dsw50250@users.noreply.github.com>
2023-06-09 13:09:31 +0200

    main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import xml.etree.ElementTree as ET
 import json
 import yaml
 
+
 def convert_xml_to_json(file_path, output_path):
     tree = ET.parse(file_path)
     root = tree.getroot()
@@ -12,6 +13,7 @@ def convert_xml_to_json(file_path, output_path):
     with open(output_path, 'w') as outfile:
         json.dump(data, outfile)
 
+        
 def convert_json_to_xml(file_path, output_path):
     with open(file_path) as json_file:
         data = json.load(json_file)
