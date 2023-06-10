827100e2afb30602e1475a3f728353baddba55e1
dsw50250 <129311304+dsw50250@users.noreply.github.com>
2023-06-10 12:55:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-import sys
-import xml.etree.ElementTree as ET
-import json
-import yaml
-
-
 def convert_xml_to_json(file_path, output_path):
     tree = ET.parse(file_path)
     root = tree.getroot()
@@ -13,7 +7,6 @@ def convert_xml_to_json(file_path, output_path):
     with open(output_path, 'w') as outfile:
         json.dump(data, outfile)
 
-        
 def convert_json_to_xml(file_path, output_path):
     with open(file_path) as json_file:
         data = json.load(json_file)
@@ -23,37 +16,3 @@ def convert_json_to_xml(file_path, output_path):
         child.text = value
     tree = ET.ElementTree(root)
     tree.write(output_path)
-
-def convert_yaml_to_json(file_path, output_path):
-    with open(file_path) as yaml_file:
-        data = yaml.safe_load(yaml_file)
-    with open(output_path, 'w') as outfile:
-        json.dump(data, outfile)
-
-def convert_json_to_yaml(file_path, output_path):
-    with open(file_path) as json_file:
-        data = json.load(json_file)
-    with open(output_path, 'w') as outfile:
-        yaml.safe_dump(data, outfile)
-
-def convert_files(input_file_path, output_file_path):
-    if input_file_path.endswith('.xml') and output_file_path.endswith('.json'):
-        convert_xml_to_json(input_file_path, output_file_path)
-    elif input_file_path.endswith('.json') and output_file_path.endswith('.xml'):
-        convert_json_to_xml(input_file_path, output_file_path)
-    elif input_file_path.endswith('.yaml') and output_file_path.endswith('.json'):
-        convert_yaml_to_json(input_file_path, output_file_path)
-    elif input_file_path.endswith('.json') and output_file_path.endswith('.yaml'):
-        convert_json_to_yaml(input_file_path, output_file_path)
-    else:
-        print("Nieobsługiwany format pliku!")
-
-if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Sposób użycia: program.exe pathFile1.x pathFile2.y")
-        sys.exit(1)
-
-    input_file_path = sys.argv[1]
-    output_file_path = sys.argv[2]
-
-    convert_files(input_file_path, output_file_path)
